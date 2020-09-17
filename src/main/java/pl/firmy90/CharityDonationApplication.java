147d538ca14234fc97ea4f9e48fe7b1ee6c5bad7
firmy90 <firmy90@protonmail.com>
2020-09-17 21:40:00 +0200

    initial commit

---
+++ src/main/java/pl/firmy90/CharityDonationApplication.java
@@ -0,0 +1,13 @@
+package pl.firmy90;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class CharityDonationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CharityDonationApplication.class, args);
+    }
+
+}
